86a792eb514ce789146ad9babb72d309079cd9c6
CvetelinaS <tsvetelina.subova@email.cz>
2021-02-16 14:00:28 +0100

    New ones

--- Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
+++ Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
@@ -14,7 +14,7 @@ import static com.cvetelina.book.DriverInit.initializeDriver;
 public class HomePage {
 
   private static WebDriver driver;
-
+  public static final String UL_XPATH = "//*[contains(@class,'mainbody')]/ul//li";
   /**
    * Pridal jsem do POM TestNg anotace. Takze misto jUnit budeme pouzivat TestNg. To nam dava moznost pouzivat i Asserty atp. Je to obdoba jUnit.
    * Tato metoda udela to, ze nainicializuje chrome driver a vstoupi na domovskou stranku. Presne nejak takto muze vypadat setup testu co napises testu.
@@ -25,46 +25,48 @@ public class HomePage {
     goToHomePage(driver);
   }
 
-  /**
-   * Takto muzes psat testy. Tady je jen ilustrovano, jak pracovat s tim driverem. Je to stejny WebDriver jako znas ze svych testu.
-   * Krok --> bez na google je samozrejme blbost :)
-   * Pokud budeme mit vic testovacich scenaru, pridame dalsi metodu s anotaci Test.
-   */
   @Test
-  public void homePageTest() {
-    //test bude zde.
-    //jen zkouším
-    WebElement inputField = driver.findElement(By.id("q"));
-    inputField.sendKeys("Cvetelina");
+  public void displayInputTest() {
+      WebElement inputField = driver.findElement(By.id("q"));
+      Assert.assertEquals(true, inputField.isDisplayed());
+      inputField.sendKeys("Cvetelina");
   }
 
   @Test
-  public void findUrls() {
-    List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter8");
-    System.out.println(expectedNames);
-
-    List<WebElement> liElements = driver.findElements(By.xpath(
-        "//ul/li")); //tady tohle funguje, nicmene bych zkusil zapsat trosku lepsi cestu. Tyto elementy maji nejaky parent div. 2. Je mozne, ze tu cestu budes potrebovat pouzit opakovane. Vyrobil bych konstantu, kde tu cestu definujes a do kodu ji budes jen predavat.
-    for (int i = 0; i < expectedNames.size(); i++) {
-      Assert.assertEquals(liElements.get(i).getText(), expectedNames.get(i));
+  public void findUrlsTest() {
+      List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter8");
+        System.out.println(expectedNames);
+        //*[contains(@id,'59')]/ul//li
+      List<WebElement> liElements = driver.findElements(By.xpath(UL_XPATH));
+        for (int i = 0; i < expectedNames.size(); i++) {
+        Assert.assertEquals(liElements.get(i).getText(), expectedNames.get(i));
     }
-
-    //List<WebElement> liElements = driver.findElements(By.xpath("//ul/li"));
-    //System.out.println(liElements.size());
-    //for (int i = 1; i < liElements.size()+1; i++) {
-    // WebElement linkElement = driver.findElement(By.xpath("//ul/li[" + i + "]/a"));
-
-    //System.out.println(linkElement.getText());
-    //Assert.assertEquals(expected, liElements.gettext());
-    //Assert.assertEquals(expected, liElements);
   }
 
+  @Test
+  public void displayHeadingTest(){
+      String expectedHeading = "Selenium: Beginners Guide";
+      String actualHeading = driver.getTitle();
+      WebElement heading = driver.findElement(By.className("mainheading"));
+      Assert.assertEquals(expectedHeading, actualHeading);
+  }
+
+  //@Test
+  //public void displayHeadingTest(){
+      //String expectedHeading = "Selenium: Beginners Guide";
+      //String actualHeading = driver.getTitle();
+      //WebElement secondHeading = driver.findElement(By.id("abId0.2997272623719913"));
+      //Assert.assertEquals(expectedHeading, actualHeading);
+      //WebElement secondHeading = driver.findElement(By.id("abId0.2997272623719913"));
+      //Assert.assertTrue(secondHeading.contains());
+    //}
   /**
    * Takto se po testech uklizi. Pokud je potreba. Muzeme treba zavrit okno, atp.
    */
   @AfterClass
   public void cleanUp() {
     //not needed right now
+    //  driver.close();
   }
 
 }
