2719b4263f20418d9d43e8e341038963bfc2caa1
CvetelinaS <tsvetelina.subova@email.cz>
2021-02-19 19:30:44 +0100

    Changes and new test

--- Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
+++ Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
@@ -8,65 +8,64 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import sun.awt.*;
+
 import static com.cvetelina.book.DriverInit.goToHomePage;
 import static com.cvetelina.book.DriverInit.initializeDriver;
 
 public class HomePage {
 
-  private static WebDriver driver;
-  public static final String UL_XPATH = "//*[contains(@class,'mainbody')]/ul//li";
-  /**
-   * Pridal jsem do POM TestNg anotace. Takze misto jUnit budeme pouzivat TestNg. To nam dava moznost pouzivat i Asserty atp. Je to obdoba jUnit.
-   * Tato metoda udela to, ze nainicializuje chrome driver a vstoupi na domovskou stranku. Presne nejak takto muze vypadat setup testu co napises testu.
-   */
-  @BeforeClass
-  public void beforeClassSetup() {
-    driver = initializeDriver();
-    goToHomePage(driver);
-  }
-
-  @Test
-  public void displayInputTest() {
-      WebElement inputField = driver.findElement(By.id("q"));
-      Assert.assertEquals(true, inputField.isDisplayed());
-      inputField.sendKeys("Cvetelina");
-  }
-
-  @Test
-  public void findUrlsTest() {
-      List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter8");
-        System.out.println(expectedNames);
-        //*[contains(@id,'59')]/ul//li
-      List<WebElement> liElements = driver.findElements(By.xpath(UL_XPATH));
-        for (int i = 0; i < expectedNames.size(); i++) {
-        Assert.assertEquals(liElements.get(i).getText(), expectedNames.get(i));
+    private static WebDriver driver;
+    public static final String CHAPTERS_LIST = "//*[contains(@class,'mainbody')]/ul//li";
+    
+    @BeforeClass
+    public void beforeClassSetup() {
+        driver = initializeDriver();
+        goToHomePage(driver);
     }
-  }
 
-  @Test
-  public void displayHeadingTest(){
-      String expectedHeading = "Selenium: Beginners Guide";
-      String actualHeading = driver.getTitle();
-      WebElement heading = driver.findElement(By.className("mainheading"));
-      Assert.assertEquals(expectedHeading, actualHeading);
-  }
+    @Test
+    public void displayInputTest() {
+        WebElement inputField = driver.findElement(By.id("q"));
+        Assert.assertTrue(inputField.isDisplayed());
+        inputField.sendKeys("Cvetelina");
+    }
 
-  //@Test
-  //public void displayHeadingTest(){
-      //String expectedHeading = "Selenium: Beginners Guide";
-      //String actualHeading = driver.getTitle();
-      //WebElement secondHeading = driver.findElement(By.id("abId0.2997272623719913"));
-      //Assert.assertEquals(expectedHeading, actualHeading);
-      //WebElement secondHeading = driver.findElement(By.id("abId0.2997272623719913"));
-      //Assert.assertTrue(secondHeading.contains());
-    //}
-  /**
-   * Takto se po testech uklizi. Pokud je potreba. Muzeme treba zavrit okno, atp.
-   */
-  @AfterClass
-  public void cleanUp() {
-    //not needed right now
-    //  driver.close();
-  }
+    @Test
+    public void findUrlsTest() {
+        List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter8");
+        System.out.println(expectedNames);
+        List<WebElement> liElements = driver.findElements(By.xpath(CHAPTERS_LIST));
+        for (int i = 0; i < expectedNames.size(); i++) {
+            Assert.assertEquals(liElements.get(i).getText(), expectedNames.get(i));
+        }
+    }
+
+    @Test
+    public void displayHeadingTest(){
+        String expectedHeading = "Selenium: Beginners Guide";
+        String actualHeading = driver.getTitle();
+        Assert.assertEquals(expectedHeading, actualHeading);
+    }
+
+    @Test
+    public void displaySubHeadingTest(){
+        List <WebElement> SubHeading = driver.findElements(By.xpath("//*[@class='mainheading']//following-sibling::div"));
+        String[]firstElement = new String[SubHeading.size()];
+        int i=0;
+        for(WebElement element: SubHeading) {
+            firstElement[i] = element.getText();
+            i++;
+            //System.out.println(firstElement[0].contains("Below is a list of links to the examples needed in the chapters. Click on the links below and follow the steps in the book."));
+            Assert.assertTrue(firstElement[0].contains("Below is a list of links to the examples needed in the chapters. Click on the links below and follow the steps in the book."));
+        }
+    }
+
+
+    @AfterClass
+    public void cleanUp() {
+        //not needed right now
+        //  driver.close();
+    }
 
 }
