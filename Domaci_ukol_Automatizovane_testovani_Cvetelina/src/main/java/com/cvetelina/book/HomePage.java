1ad3189f66bb908b69f4e476122e1e872cc97acc
CvetelinaS <tsvetelina.subova@email.cz>
2021-02-21 09:31:58 +0100

    Change to the first test

--- Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
+++ Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
@@ -33,7 +33,7 @@ public class HomePage {
 
     @Test
     public void findUrlsTest() {
-        List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter8");
+        List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter5");
         System.out.println(expectedNames);
         List<WebElement> liElements = driver.findElements(By.xpath(CHAPTERS_LIST));
         for (int i = 0; i < expectedNames.size(); i++) {
