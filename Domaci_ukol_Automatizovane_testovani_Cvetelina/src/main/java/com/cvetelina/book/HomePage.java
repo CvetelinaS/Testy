cea00d02923057bdfb70dda050af82d46af9481d
Daniel Bechny <daniel.bechny@blackboard.com>
2021-02-25 15:14:52 +0100

    Reviewed tests and introduced problem to crack.

--- Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
+++ Domaci_ukol_Automatizovane_testovani_Cvetelina/src/main/java/com/cvetelina/book/HomePage.java
@@ -1,7 +1,9 @@
 package com.cvetelina.book;
 
 import java.util.*;
+
 import org.openqa.selenium.*;
+
 import org.testng.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,9 +15,10 @@ import static com.cvetelina.book.DriverInit.initializeDriver;
 public class HomePage {
 
   private static WebDriver driver;
-  public static final String CHAPTERS_LIST = "//*[contains(@class,'mainbody')]/ul//li";
+  public static final String CHAPTERS_LIST = "//*[contains(@class,'mainbody')]/ul//li"; // make private
+  private static final String SUB_HEADING = "Below is a list of links to the examples needed in the chapters. Click on the links below and follow the steps in the book.";
 
-   // Tohle je komentar 
+  // Tohle je komentar
   @BeforeClass
   public void beforeClassSetup() {
     driver = initializeDriver();
@@ -32,36 +35,36 @@ public class HomePage {
   @Test
   public void findUrlsTest() {
     List<String> expectedNames = Arrays.asList("Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter5");
+    System.out.println("Expected names:");
     System.out.println(expectedNames);
     List<WebElement> liElements = driver.findElements(By.xpath(CHAPTERS_LIST));
+    System.out.println("Actual names");
+    System.out.println(liElements);
     for (int i = 0; i < expectedNames.size(); i++) {
-      Assert.assertEquals(liElements.get(i).getText(), expectedNames.get(i));
+      Assert.assertEquals(liElements.get(i).getText(), expectedNames.get(i), "Expected names do not match actual.");
     }
   }
 
   @Test
-  public void displayHeadingTest(){
+  public void displayHeadingTest() {
     String expectedHeading = "Selenium: Beginners Guide";
-    String actualHeading = driver.getTitle();
+    String actualHeading = driver.getTitle(); // Checkujes nadpis tabu, ne nadpis stranky.
     Assert.assertEquals(expectedHeading, actualHeading);
   }
 
   @Test
-  public void displaySubHeadingTest(){
+  public void displaySubHeadingTest() {
+    //nazev listu s malym
     List<WebElement> SubHeading = driver.findElements(By.xpath("//*[@class='mainheading']//following-sibling::div"));
-    String[]firstElement = new String[SubHeading.size()];
-    int i=0;
-    for(WebElement element: SubHeading) {
-      firstElement[i] = element.getText();
-      i++;
-      //System.out.println(firstElement[0].contains("Below is a list of links to the examples needed in the chapters. Click on the links below and follow the steps in the book."));
-      Assert.assertTrue(firstElement[0].contains("Below is a list of links to the examples needed in the chapters. Click on the links below and follow the steps in the book."));
-    }
+    WebElement element = SubHeading.get(0);
+    String elementText = element.getText();
+    // Find way how to extract only desired text from elementText String. Desired text equal SUB_HEADING.
+    Assert.assertEquals(element.getText(), SUB_HEADING);
   }
 
   @AfterClass
   public void cleanUp() {
-    //not needed right now
+    // consider adding window closing.
   }
 
 }
